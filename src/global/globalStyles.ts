7c519ab24dc851c638452418cb7efdfe41a9dd2f
jisooround <9utesleben@gmail.com>
2023-06-29 15:03:44 +0900

    env: favicon, title 설정 & 전역 스타일 설정

---
+++ src/global/globalStyles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+
+:root {
+  --color-blue : #0085FF;
+  --color-black90: #262626;
+  --color-black80: #3C3C3C;
+  --color-black70: #515151;
+  --color-black60: #676767;
+  --color-black50: #7D7D7D;
+  --color-black40: #A8A8A8;
+  --color-black30: #BEBEBE;
+  --color-black20: #D4D4D4;
+  --color-black10: #E9E9E9;
+  --color-gray: #F2F2F2;
+  --color-white: #FFFFFF;
+}
+
+body {
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-blue) ;
+}
+`;
+
+export default GlobalStyles;
