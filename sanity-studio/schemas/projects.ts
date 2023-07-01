88ddddee808ba4e7efd51d510ae6c5ff8c1a52dc
jisooround <9utesleben@gmail.com>
2023-07-01 23:59:26 +0900

    feat: project 데이터 수정

---
+++ sanity-studio/schemas/projects.ts
@@ -0,0 +1,68 @@
+export default {
+  name: 'projects', // 데이터의 이름
+  type: 'document', // 데이터의 타입
+  title: 'Project', // sanity studio에서 보여주는 이름, 첫 글자는 대문자가 일반적.
+  fields: [
+    // 필드를 설명하는 개체 배열
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'order',
+      type: 'number',
+      title: 'order',
+    },
+    {
+      name: 'icon',
+      type: 'string',
+      title: 'Icon',
+    },
+    {
+      name: 'tool',
+      type: 'array',
+      title: 'Tool',
+      of: [
+        {
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'content',
+      type: 'string',
+      title: 'Content',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
+    {
+      name: 'githuburl',
+      type: 'string',
+      title: 'GitHubURL',
+    },
+    {
+      name: 'blogurl',
+      type: 'string',
+      title: 'BlogURL',
+    },
+    {
+      name: 'datestart',
+      type: 'date',
+      title: 'Date(start)',
+    },
+    {
+      name: 'dateend',
+      type: 'date',
+      title: 'Date(end)',
+    },
+    {
+      name: 'team',
+      type: 'string',
+      title: 'Team',
+    },
+  ],
+}
