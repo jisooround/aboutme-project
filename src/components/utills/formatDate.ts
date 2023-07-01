cdfe82169e9041c9c6d591dc2106d831abe7326d
jisooround <9utesleben@gmail.com>
2023-07-01 23:56:37 +0900

    feat: 요일 포맷 함수

---
+++ src/components/utills/formatDate.ts
@@ -0,0 +1,7 @@
+export function formatDate(value: string): string {
+  const date = new Date(value);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}년 ${month}월 ${day}일`;
+}
