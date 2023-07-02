6dc2e02f1ebe02fc0e7562f69acc47cc8bbb02e6
jisooround <9utesleben@gmail.com>
2023-07-03 00:37:28 +0900

    feat: Contact 페이지

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import styled from "styled-components";
+import { LuExternalLink } from "react-icons/lu";
+
+const ContactCard = () => {
+  const blogURL = "https://velog.io/@mudidu";
+  const gitURL = "https://github.com/jisooround";
+
+  function clickLink(url: string) {
+    window.open(url);
+  }
+  return (
+    <CardStyle>
+      <NameAreaStyle>
+        <h3>우지수</h3>
+      </NameAreaStyle>
+      <InfoAreaStyle>
+        <div>
+          <p>Phone</p>
+          <p>010 5028 7344</p>
+        </div>
+        <div>
+          <p>Email</p>
+          <p>9utesleben@gmail.com</p>
+        </div>
+        <div>
+          <p>GitHub</p>
+          <p className="pointer" onClick={() => clickLink(gitURL)}>
+            Link <LuExternalLink />
+          </p>
+        </div>
+        <div>
+          <p>Blog</p>
+          <p className="pointer" onClick={() => clickLink(blogURL)}>
+            Link <LuExternalLink />
+          </p>
+        </div>
+      </InfoAreaStyle>
+    </CardStyle>
+  );
+};
+
+const CardStyle = styled.div`
+  width: 35%;
+  min-width: 380px;
+  aspect-ratio: 90 / 53;
+  margin: 80px;
+  background-color: var(--color-white);
+  box-sizing: border-box;
+  padding: 2.1875rem;
+  color: var(--color-blue);
+  display: flex;
+  background-image: url("./strokeLogo.png");
+  background-position: -10% 300%;
+  background-size: 80%;
+  background-repeat: no-repeat;
+  @media ${(props) => props.theme.laptop} {
+    flex-direction: column;
+    justify-content: space-between;
+    aspect-ratio: 3 / 4;
+    background-position: 90% 10%;
+    background-size: 80%;
+  }
+`;
+
+const NameAreaStyle = styled.section`
+  width: 50%;
+  h3 {
+    font-weight: 400;
+    font-size: 32px;
+    padding-bottom: 1rem;
+  }
+`;
+
+const InfoAreaStyle = styled.section`
+  width: 60%;
+  div {
+    display: grid;
+    grid-template-columns: 20% 80%;
+    gap: 1.25rem;
+    padding-bottom: 12px;
+    &:last-child {
+      padding-bottom: 0;
+    }
+    p {
+      display: flex;
+      font-size: 1.3125rem;
+      font-weight: 300;
+      gap: 0.3125rem;
+      &:nth-child(1) {
+        font-weight: 500;
+      }
+      &.pointer {
+        cursor: pointer;
+      }
+    }
+    @media ${(props) => props.theme.laptop} {
+      grid-template-columns: 100%;
+      gap: 0.1875rem;
+      padding-bottom: 1.25rem;
+      p {
+        font-size: 20px;
+      }
+    }
+  }
+`;
+
+export default ContactCard;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import { LuExternalLink } from "react-icons/lu";
+import ContactCard from "@/components/ContactCard";
+import ContactSwiper from "@/components/ContactSwiper";
 
 const Contact = () => {
-  return <ContactStyle>asdf</ContactStyle>;
+  return (
+    <ContactStyle>
+      <ContactCard />
+      <h1>Here is some information about me.</h1>
+    </ContactStyle>
+  );
 };
 
 const ContactStyle = styled.div`
-  padding-top: 100px;
-  box-sizing: border-box;
-  width: 800px;
-  height: 600px;
-  margin: 0 auto;
-  background-color: var(--color-white);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 65vh;
+  h1 {
+    font-style: italic;
+    position: absolute;
+    bottom: 1.875rem;
+    font-size: 3rem;
+  }
 `;
 
 export default Contact;
