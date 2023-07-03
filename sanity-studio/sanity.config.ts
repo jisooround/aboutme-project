e267148e1361afd7a05a93e5391b1f0bf92b4a94
jisooround <9utesleben@gmail.com>
2023-07-03 15:46:46 +0900

    fix: redirects

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -3,8 +3,6 @@ import {visionTool} from '@sanity/vision'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
 import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
-import {dashboardTool} from '@sanity/dashboard'
-import {netlifyWidget} from 'sanity-plugin-dashboard-widget-netlify'
 
 const devOnlyPlugins = [getStartedPlugin()]
 
@@ -15,33 +13,7 @@ export default defineConfig({
   projectId: 'hoxtcsyy',
   dataset: 'production',
 
-  plugins: [
-    deskTool(),
-    visionTool(),
-    ...(isDev ? devOnlyPlugins : []),
-    dashboardTool({
-      widgets: [
-        netlifyWidget({
-          title: 'My Netlify deploys',
-          sites: [
-            {
-              title: 'Sanity Studio',
-              apiId: 'xxxxx-yyyy-zzzz-xxxx-yyyyyyyy',
-              buildHookId: 'xxxyyyxxxyyyyxxxyyy',
-              name: 'sanity-gatsby-blog-20-studio',
-            },
-            {
-              title: 'Website',
-              apiId: '8c0c1e35-af95-48c5-8b3c-c0aa0f6be93e',
-              buildHookId: '5c23354f454e1350f8543e78',
-              name: 'dashing-biscuit-b443b1',
-              url: 'https://dashing-biscuit-b443b1.netlify.app/projects',
-            },
-          ],
-        }),
-      ],
-    }),
-  ],
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
 
   schema: {
     types: schemaTypes,
