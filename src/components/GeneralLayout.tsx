72fa2f567bfc9448cbee9466b32465318e53e12f
jisooround <9utesleben@gmail.com>
2024-03-13 18:16:29 +0900

    add: contact popup페이지 추가

--- src/components/GeneralLayout.tsx
+++ src/components/GeneralLayout.tsx
@@ -1,23 +1,32 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navbar from "./Navbar";
+import ContactPopup from "./ContactPopup";
+import { RxEyeNone } from "react-icons/rx";
 
 type Props = {
   children: React.ReactNode;
+  contactFlag: boolean;
 };
 
 const GeneralLayout = ({ children }: Props) => {
+  const [contactFlag, setContactFlag] = useState<boolean>(false);
+  useEffect(() => {
+    console.log(contactFlag);
+  }, [contactFlag]);
+
   return (
-    <GeneralLayoutStyle>
-      <Navbar />
+    <GeneralLayoutStyle contactFlag={contactFlag}>
+      {contactFlag && <ContactPopup setContactFlag={setContactFlag} />}
+      <Navbar setContactFlag={setContactFlag} />
       <GeneralBodyStyle>{children}</GeneralBodyStyle>
     </GeneralLayoutStyle>
   );
 };
 
-const GeneralLayoutStyle = styled.div`
+const GeneralLayoutStyle = styled.div<Props>`
   width: 100%;
-  /* padding-bottom: 9.375rem; */
+  overflow: ${(props) => (props.contactFlag ? "auto" : "")};
 `;
 
 const GeneralBodyStyle = styled.div`
