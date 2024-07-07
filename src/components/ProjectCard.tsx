410f813d98a0a099b00b12d6d6f9baf1c6264703
jisooround <9utesleben@gmail.com>
2024-07-07 16:39:12 +0900

    feat: 프로젝트 리스트 > 카드 UI 스타일

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,6 +2,8 @@ import { ProjectsListItemType } from "@/model/project";
 import { useState } from "react";
 import styled from "styled-components";
 import { HiOutlinePlus } from "react-icons/hi2";
+import { FaGithub } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
 
 type Props = {
   item: ProjectsListItemType;
@@ -14,15 +16,29 @@ const ProjectCard = ({ item }: Props) => {
   return (
     <>
       <CardStyle $isOpen={isOpenInfo}>
-        <TitleWrap>{title}</TitleWrap>
-        <IconWrap
-          $isOpen={isOpenInfo}
-          onClick={() => {
-            setIsOpenInfo((prev) => !prev);
-          }}
-        >
-          <HiOutlinePlus className="icon" />
-        </IconWrap>
+        <TitleArea>
+          <TitleWrap>{title}</TitleWrap>
+          <IconWrap
+            $isOpen={isOpenInfo}
+            onClick={() => {
+              setIsOpenInfo((prev) => !prev);
+            }}
+          >
+            <HiOutlinePlus className="icon" />
+          </IconWrap>
+        </TitleArea>
+        {isOpenInfo && (
+          <ContentArea>
+            <Icon>
+              <p>CODE</p>
+              <FaGithub />
+            </Icon>
+            <Icon>
+              <p>VIEW</p>
+              <FaRegEye />
+            </Icon>
+          </ContentArea>
+        )}
       </CardStyle>
     </>
   );
@@ -30,15 +46,20 @@ const ProjectCard = ({ item }: Props) => {
 
 const CardStyle = styled.div<{ $isOpen: boolean }>`
   width: 100%;
-  height: ${(props) => (props.$isOpen ? "300px" : "auto")};
-  transition: height 0.3s ease;
+  /* height: ${(props) => (props.$isOpen ? "300px" : "auto")}; */
+  height: auto;
   box-sizing: border-box;
   border-bottom: 1px solid var(--color-black30);
   padding: 20px 1.25rem;
   cursor: pointer;
   display: flex;
+  flex-wrap: wrap;
+`;
+
+const TitleArea = styled.div`
+  width: 100%;
+  display: flex;
   justify-content: space-between;
-  transition: transform 0.3s ease;
 `;
 
 const TitleWrap = styled.div`
@@ -58,6 +79,46 @@ const IconWrap = styled.div<{ $isOpen: boolean }>`
   .icon {
     transform: ${(props) => (props.$isOpen ? "rotate(45deg)" : "rotate(0)")};
     transition: transform 0.2s ease;
+  }
+`;
+
+const ContentArea = styled.div`
+  width: 100%;
+  height: 30px;
+  animation-name: animate-appear;
+  animation-duration: 0.5s;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 0.5rem;
+  @keyframes animate-appear {
+    0% {
+      opacity: 0;
+      height: 0px;
+    }
+    100% {
+      opacity: 1;
+      height: 30px;
+    }
+  }
+`;
+
+const Icon = styled.div`
+  width: 100px;
+  height: 30px;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: var(--color-white);
+  background-color: var(--color-black90);
+  border-radius: 0.375rem;
+  padding: 0.625rem;
+  box-sizing: border-box;
+  &:hover {
+    background-color: var(--color-white);
+    color: var(--color-black90);
+    transition: all 0.2s ease;
   }
 `;
 export default ProjectCard;
